ab8994988e77fb33b5974a5242f774f779191c1c
JMRFB <bodinojomar418@gmail.com>
2026-01-20 16:51:17 +0800

    Apply Sidebar to Profile

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,48 +16,13 @@
         <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
     </head>
-    <body class="font-sans antialiased">
-        <div class="relative min-h-screen md:flex" x-data="{ open: true }">
+    <body class="font-sans antialiased min-h-screen h-full">
+        <div class="relative min-h-fit md:flex h-fit" x-data="{ open: true }">
 
             <!-- sidebar -->
-            <aside x-cloak :class="{ '-translate-x-full': !open }" class="z-10 bg-blue-800 text-blue-100 w-64 px-2 py-4 absolute inset-y-0 left-0 md:relative transform -translate-x-full md:translate-x-0
-            overflow-y-auto transition ease-in-out duration-300 shadow-lg">
-            <!-- logo -->
-                <div class="flex items-center justify-between px-2">
-                    <div class="flex items-center gap-2"> 
-                        <a href="">
-                            @include('components.application-logo')
-                        </a>
-                        <span class="text-2xl font-extra-bold">
-                            Admin
-                        </span>
-                    </div>
-                    <button type="button" @click="open = !open" class="lg:hidden inline-flex p-2 items-center justify-center rounded-md text-blue-100 hover:bg-blue-700 focus:outline-none">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                        </svg>
-                    </button>
-                </div>
-
-                <!-- Nav Links -->
-                <nav class="mt-4 flex flex-col gap-2">
-                    <x-side-nav-link href="{{ URL('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        Dashboard
-                    </x-side-nav-link>
-                    <x-side-nav-link href="{{ URL('features') }}" :active="request()->routeIs('features')">
-                        Features
-                    </x-side-nav-link>
-                    <x-side-nav-link href="{{ URL('about') }}" :active="request()->routeIs('about')">
-                        About
-                    </x-side-nav-link>
-                    <x-side-nav-link href="{{ URL('contact') }}" :active="request()->routeIs('contact')">
-                        Contact
-                    </x-side-nav-link>
-                </nav>
-            </aside>
-    
+            @include('layouts.sideBar')
             <!-- main content -->
-            <main class="flex-1 bg-gray-100 h-screen">
+            <main class="flex-1 bg-gray-100 min-h-screen h-fit">
                 <nav class="bg-blue-900 shadow-lg">
                    <div class="mx-auto px-2 sm:px-6 lg:px-8">
                         <div class="flex item-center justify-between md:justify-end h-16">
@@ -85,16 +50,13 @@
                                             </div>
                                         </button>
                                     </x-slot>
-
                                     <x-slot name="content">
                                         <x-dropdown-link :href="route('profile.edit')">
                                             {{ __('Profile') }}
                                         </x-dropdown-link>
-
                                         <!-- Authentication -->
                                         <form method="POST" action="{{ route('logout') }}">
                                             @csrf
-
                                             <x-dropdown-link :href="route('logout')"
                                                     onclick="event.preventDefault();
                                                                 this.closest('form').submit();">
---
+++ resources/views/layouts/sideBar.blade.php
@@ -0,0 +1,37 @@
+
+<aside x-cloak :class="{ '-translate-x-full': !open }" class="z-10 bg-blue-800 text-blue-100 w-64 px-2 py-4 absolute inset-y-0 left-0 md:relative transform -translate-x-full md:translate-x-0
+    overflow-y-auto transition ease-in-out duration-300 shadow-lg">
+    
+    <!-- logo -->
+    <div class="flex items-center justify-between px-2">
+        <div class="flex items-center gap-2"> 
+            <a href="">
+                @include('components.application-logo')
+            </a>
+            <span class="text-2xl font-extra-bold">
+                Admin
+            </span>
+        </div>
+        <button type="button" @click="open = !open" class="lg:hidden inline-flex p-2 items-center justify-center rounded-md text-blue-100 hover:bg-blue-700 focus:outline-none">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
+        </button>
+    </div>
+
+    <!-- Nav Links -->
+    <nav class="mt-4 flex flex-col gap-2">
+        <x-side-nav-link href="{{ URL('dashboard') }}" :active="request()->routeIs('dashboard')">
+            Dashboard
+        </x-side-nav-link>
+        <x-side-nav-link href="{{ URL('features') }}" :active="request()->routeIs('features')">
+            Features
+        </x-side-nav-link>
+        <x-side-nav-link href="{{ URL('about') }}" :active="request()->routeIs('about')">
+            About
+        </x-side-nav-link>
+        <x-side-nav-link href="{{ URL('contact') }}" :active="request()->routeIs('contact')">
+            Contact
+        </x-side-nav-link>
+    </nav>
+</aside>
